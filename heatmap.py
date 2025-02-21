288c42f0c5f286d298d92695c5eceba4fb9ca29f
Amin-mat <seyyedamin1381@gmail.com>
2025-02-21 14:57:37 +0330

    Add files via upload

--- heatmap.py
+++ heatmap.py
@@ -1,4 +1,32 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+# Replace with your actual GTM ID
+GTM_ID = "GTM-P8FDLQQB"
+
+# GTM script (head)
+GTM_JS = f"""
+<script>
+(function(w,d,s,l,i){{w[l]=w[l]||[];w[l].push({{"gtm.start":
+new Date().getTime(),event:"gtm.js"}});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+}})(window,document,'script','dataLayer','{GTM_ID}');
+</script>
+"""
+
+# GTM noscript (body)
+GTM_NOSCRIPT = f"""
+<noscript>
+<iframe src="https://www.googletagmanager.com/ns.html?id={GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+"""
+
+# Inject GTM script
+components.html(GTM_JS, height=0)
+components.html(GTM_NOSCRIPT, height=0)
+
 import pandas as pd
 import numpy as np
 import json
